46efcd273a4f995500eed2ec5a083f7bb2a5412d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-25 02:56:11 +0530

    TimerForm Component added

--- public/js/app.js
+++ public/js/app.js
@@ -67,3 +67,35 @@ class EditableTimer extends React.Component {
     }
   }
 }
+
+class TimerForm extends React.Component {
+  render() {
+    // this.props.title to determine what text the submit button at the bottom of the form should display. If title is present, we know we're editing an existing timer, so it displays update. Otherwise it displays "Create".
+    const submitText = this.props.title ? 'Update' : 'Create';
+    return (
+      <div className="ui centered card">
+        <div className="content">
+          <div className="ui form">
+            <div className="field">
+              <label>Title</label>
+              <input type="text" defaultValue={this.props.title} />
+            </div>
+            <div className="field">
+              <label>Project</label>
+              {/* defaultValue - React Property, when the form is used for editing as it is here, this set the fields to current values of the timer as desired */}
+              <input type="text" defaultValue={this.props.project} />
+            </div>  
+            <div className="ui two bottom attached buttons">
+              <button className="ui basic blue button">
+                {submitText}
+              </button>
+              <button className="ui basic red button">
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
