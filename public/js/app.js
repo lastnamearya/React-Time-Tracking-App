54ef270698fdd979ea95e9fd5d47496a0ce49ca2
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-28 10:14:56 +0530

    Initial state added to our app ~ Hard Code Values

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,34 @@
 class TimersDashboard extends React.Component {
+  state = {
+    timers: [
+      {
+        title: "Practice squat",
+        project: "Gym Chores",
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title: "Bake squash",
+        project: "Kitchen Chores",
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null,
+      },
+    ],
+  };
+
   render() {
     return (
       <div className="ui three column centered grid">
         <div className="column">
         {/* Main Parent Component have two Sub-Child Components */}
         {/* Component for Showing Timers */}
-          <EditableTimerList />
-          {/* Component for Showing + Button */}
-          <ToggleableTimerForm
-          // Passing additional Prop isOpen={true}, when true display the form to create new timer
-            isOpen={false}
+          <EditableTimerList 
+            timers={this.state.timers}
           />
+          {/* Component for Showing + Button */}
+          <ToggleableTimerForm />
         </div>
       </div>
     );
@@ -37,6 +55,7 @@ class EditableTimerList extends React.Component {
           project="World Domination"
           elapsed="3890985"
           runningSince={null}
+          // This is will decide, what to display --> Timer, Timer Form
           editFormOpen={true}
         />
       </div>
